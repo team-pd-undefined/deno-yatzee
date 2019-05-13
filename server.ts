6542dd92bfe3494dc2efefe2eeafddf22ddc5cc2
yhc0527 <yhc0527@gmail.com>
2019-05-13 15:08:15 +0900

    first commit

---
+++ server.ts
@@ -0,0 +1,31 @@
+import { serve } from "https://deno.land/std/http/server.ts";
+
+class SsemiServer {
+    async listen(port: number, callback: Function) {
+        try {
+            const s = serve(`0.0.0.0:${port}`);
+            callback();
+            for await (const request of s) {
+                if(request.url === "/home") {
+                    request.respond({ body: new TextEncoder().encode("Hello Home\n") });
+                } else if(request.url === "/page") {
+                    request.respond({ body: new TextEncoder().encode("Hello Page\n") });
+                } else {
+                    request.respond({ body: new TextEncoder().encode("Hello Root\n") });
+                }
+            }
+        } catch(e) {
+            console.log(e);
+        }
+    }
+}
+
+function main() {
+    const port = Number.parseInt(Deno.args[1]) || 8000;
+    const server = new SsemiServer();
+    server.listen(port, () => {
+        console.log(`Listen to ${port}`)
+    });
+}
+
+main();
